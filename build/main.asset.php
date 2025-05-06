1d7652f9f68bc6d8ddad4d618f4451b298144144
Matthew Hay <matt@forefathersgroup.com>
2025-05-06 10:42:18 -0400

    update: form styles

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => 'a85dd9009c00f21312b8');
+<?php return array('dependencies' => array('jquery'), 'version' => 'b552cee8970c78720fbd');
